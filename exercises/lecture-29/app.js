cfb08bc62ef13c953caf1ad01825a3685a754f0d
Yana <nanaha28@gmail.com>
2024-04-16 19:33:02 +0300

    lecture-29

---
+++ exercises/lecture-29/app.js
@@ -0,0 +1,51 @@
+class AuthException extends Error {
+    constructor(code, message) {
+        super(message ? `${code}: ${message}` : code);
+        this.code = code;
+    }
+
+
+    toString() {
+        return this.message;
+    }
+}
+
+
+let isAuth = (auth) => auth ? ? false;
+
+let dialogBoxId = document.getElementById("dialogBox");
+
+function showDialog(e) {
+
+    dialogBoxId.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+            e.preventDefault();
+        }
+
+    });
+
+    dialogBoxId.showModal();
+}
+
+function closeDialog() {
+    dialogBoxId.close();
+}
+
+let dialogBox = dialogBoxId.querySelector(".message");
+let checkAuth = document.querySelector(".check-auth");
+
+checkAuth.addEventListener('click', function () {
+
+    try {
+        if (!isAuth()) {
+            throw new AuthException('FORBIDDEN', 'You do not have access to this page');
+        } else {
+            window.open('success.html', '_blank');
+        }
+
+    } catch (e) {
+        dialogBox.textContent = e;
+        showDialog();
+    }
+
+});
