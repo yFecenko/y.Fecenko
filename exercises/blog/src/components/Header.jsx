b7b384119dbe684aaad27f2ba23e7c3a8bb95780
Yana <nanaha28@gmail.com>
2024-04-24 15:34:23 +0300

    lecture-33

---
+++ exercises/blog/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header () {
+<>
+<h1>I am a JSX element</h1>
+<h1>Welcome to React</h1>
+  <h2>Getting Started React</h2>
+  <h3>JavaScript Library</h3>
+</>
+
+
+}
+
+export default Header;
